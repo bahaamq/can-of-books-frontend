22815a7d6bee5beb391e85f063dd67d110470825
bahaamq <bahaa.mq@gmail.com>
2021-05-30 20:15:39 +0300

    setupready

---
+++ src/Profile.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
+
+class Profile extends Component {
+    render() {
+        const { user, isAuthenticated } = this.props.auth0;
+        // return <div>Hello {user.name}</div>;
+        return (
+            <>
+                { isAuthenticated &&
+                    <>
+
+                        <div> <img src={user.picture} />
+
+                        </div>
+                        <div>Hello {user.name}</div>
+                        <div>Email: {user.email}</div>
+                    </>
+
+                }
+            </>
+        )
+    }
+}
+
+export default withAuth0(Profile);
---
+++ src/app.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
+import Header from './Header';
+
+
+import IsLoadingAndError from './IsLoadingAndError';
+import Footer from './footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
+import Login from './login';
+import MyFavoriteBooks from './myFavoriteBooks';
+import Profile from './Profile';
+
+
+class App extends React.Component {
+
+  render() {
+    const { isAuthenticated } = this.props.auth0;
+    return (
+      <>
+        <Router>
+          <IsLoadingAndError>
+            <Header />
+            <Switch>
+              <Route path="/">
+                {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
+
+
+                {isAuthenticated&& 
+                <MyFavoriteBooks />}
+
+                {!isAuthenticated && 
+                <Login />}
+
+              </Route>
+              </Switch>
+              <Switch>
+
+              <Route path="/profile">
+
+                {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+             <Profile />
+            
+
+              </Route>
+              </Switch>
+
+            <Footer />
+          </IsLoadingAndError>
+        </Router>
+      </>
+    )
+  }
+}
+
+export default  withAuth0(App);
